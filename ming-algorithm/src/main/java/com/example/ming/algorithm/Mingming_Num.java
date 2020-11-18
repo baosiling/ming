ef7d4fe858420aa7023c783755c2535b2f992fcd
nalo <1257593080@qq.com>
2020-11-18 20:09:06 +0800

    Mingming_Num

--- ming-algorithm/src/main/java/com/example/ming/algorithm/Mingming_Num.java
+++ ming-algorithm/src/main/java/com/example/ming/algorithm/Mingming_Num.java
@@ -1,6 +1,7 @@
 package com.example.ming.algorithm;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,9 +16,19 @@ public class Mingming_Num {
             }
             numList.add(Integer.parseInt(value));
         }
-        for(Integer i : numList) {
-
+        for(int i=0; i<numList.size();) {
+            int count = numList.get(i);
+            List<Integer> tempList = new ArrayList<>();
+            for(int j=i+1; j<i + count + 1; j++){
+                if(!tempList.contains(numList.get(j))){
+                    tempList.add(numList.get(j));
+                }
+            }
+            tempList.sort(Integer::compareTo);
+            for (Integer integer : tempList) {
+                System.out.println(integer);
+            }
+            i = i + count + 1;
         }
-        System.out.println(numList);
     }
 }
