1b2189c72fea8cf6881a66f15cdcee4b40565c6e
nalo <1257593080@qq.com>
2020-11-18 20:39:33 +0800

    Mingming_Num

--- ming-algorithm/src/main/java/com/example/ming/algorithm/Mingming_Num.java
+++ ming-algorithm/src/main/java/com/example/ming/algorithm/Mingming_Num.java
@@ -1,34 +1,22 @@
 package com.example.ming.algorithm;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Mingming_Num {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Integer> numList = new ArrayList<>();
-        while(true){
-            String value = scanner.nextLine();
-            if(value.equals("")){
-                break;
-            }
-            numList.add(Integer.parseInt(value));
-        }
-        for(int i=0; i<numList.size();) {
-            int count = numList.get(i);
-            List<Integer> tempList = new ArrayList<>();
-            for(int j=i+1; j<i + count + 1; j++){
-                if(!tempList.contains(numList.get(j))){
-                    tempList.add(numList.get(j));
+        while (scanner.hasNext()) {
+            TreeSet<Integer> set = new TreeSet<>();
+            int n = scanner.nextInt();
+            if (n > 0) {
+                for (int i = 0; i < n; i++) {
+                    set.add(scanner.nextInt());
                 }
             }
-            tempList.sort(Integer::compareTo);
-            for (Integer integer : tempList) {
-                System.out.println(integer);
+            for (Integer i : set) {
+                System.out.println(i);
             }
-            i = i + count + 1;
         }
     }
 }
